f9b630f4eb18aa62241a6071d7297f62d71cf642
Torben Schiz <schixix@googlemail.com>
2022-12-01 17:09:25 +0100

    finish required steps of exercise

---
+++ cmake/CPackConfig.cmake
@@ -0,0 +1,13 @@
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "SSE cpack exercise"
+  )
+set(CPACK_PACKAGE_DESCRIPTION "SSE cpack exercise long description")
+
+
+set(CPACK_PACKAGE_VENDOR "Torben Schiz")
+set(CPACK_PACKAGE_CONTACT "st161671@stud.uni-stuttgart.de")
+set(CPACK_PACKAGE_MAINTAINERS "SSE lecturers and attendends ${CPACK_PACKAGE_CONTACT}")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/tjwsch/cpack-exercise-wt2223")
+
+include(CPack)
