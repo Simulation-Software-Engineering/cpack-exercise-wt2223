e1de0ed8b70dd7f60e47a11f7569375e44ad52f0
MohamedAdelNaguib <mohamedadel1551998@gmail.com>
2022-12-01 19:49:18 +0100

    First Task of adding install targets to cmake configration

---
+++ cmake/CPackConfig.cmake
@@ -0,0 +1,10 @@
+set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "SSE cpake exercise solution"
+  CACHE STRING "Extended summary.")
+
+set(CPACK_PACKAGE_VENDOR "SSE Lecturers")
+set(CPACK_PACKAGE_CONTACT "mohamedadel1551998@gmail.com")
+set(CPACK_PACKAGE_MAINTAINERS "Mohamed Ahmed")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/MohamedAdelNaguib/cpack-exercise-wt2223")
+include(CPack)
