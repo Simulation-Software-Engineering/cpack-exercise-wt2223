eaa2188e3398cb615ec3f4c5a1805a3c7f54af28
mustafacevik <2763>
2022-12-08 00:02:00 +0100

    CPackConfig.cmake and CMakeLists.txt updated.

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -1,18 +1,18 @@
 set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
 
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "CPack Exercise Winter Semester 2022-2023"
-        CACHE STRING "Winter's summary.")
+        CACHE STRING "CPack Exercise to package code into TGZ and DEB files. Winter Semester 2022-2023")
 
-set(CPACK_PACKAGE_VENDOR "Mustafa")
+set(CPACK_PACKAGE_VENDOR "Mustafa Cevik")
 set(CPACK_PACKAGE_CONTACT "mustafacevik963@gmail.com")
 set(CPACK_PACKAGE_MAINTAINERS "SSE lectured student ${CPACK_PACKAGE_CONTACT}")
-set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/musteng")
-set(CPACK_GENERATOR "TGZ;DEB")
-
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/musteng/cpack-exercise-wt2223.git")
+set(CPACK_GENERATOR "TGZ;DEB") # Packages to be generated
 
 # Debian packaging section
 set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS YES)
 set(CPACK_STRIP_FILES TRUE)
+# Debian packaging section end
 
 include(CPack)
